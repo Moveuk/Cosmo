1584313ab9fccbb2b642a60e7bb2e6650723004d
moveuk <v_donguk@naver.com>
2022-05-29 17:02:54 +0900

    first commit

---
+++ src/test/java/com/moveuk/cosmo/CosmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moveuk.cosmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
